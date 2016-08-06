92821c8b664936d748533f2f7a42295b64fc37b4
Rhydian <Rhydz@msn.com>
2016-08-06 14:08:48 +0100

    index is now .php, unsuccessful attemp to use config.php to define paths

--- config.php
+++ config.php
@@ -1,4 +1,10 @@
 <?php
-define("ROOT_PATH", __DIR__);
+
+// Names
+define("SITE_NAME", "Test Website");
+
+// Paths
+//define("ROOT_PATH", __DIR__ . "/");
+define("ROOT_PATH", "/ws/");
 define("STYLES_PATH", ROOT_PATH . "res/css/");
 define("JS_PATH", ROOT_PATH . "res/js/");
