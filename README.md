5ebe3dba42d26ee2ab6a0be97ca72044295d6cf9
rhydianjenkins <rhydz@msn.com>
2016-08-22 19:52:08 +0100

    removed grocery images, added scripts and contact us form

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Website
+# Tata steel sailing club
+# Website by Rhydian Jenkins
+# rhydz@msn.com
--- config.php
+++ config.php
@@ -1,10 +1,11 @@
 <?php
 
 // Names
-define("SITE_NAME", "Test Website");
+define("SITE_NAME", "Tata Steel Sailing Club");
 
 // Paths#
 define("ROOT_PATH", "./");
 define("STYLES_PATH", ROOT_PATH . "res/css/");
 define("JS_PATH", ROOT_PATH . "res/js/");
 define("IMG_PATH", ROOT_PATH . "res/img/");
+define("INC_PATH", ROOT_PATH . "inc/");
