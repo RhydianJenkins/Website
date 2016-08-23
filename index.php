dcd95e92680e2bbfd39b5d2bb60bf903762dd1d2
rhydianjenkins <rhydz@msn.com>
2016-08-24 00:05:47 +0100

    font change (cant see a diff)

--- index.php
+++ index.php
@@ -23,18 +23,18 @@
 		<a class="w3-padding-large w3-hover-white w3-large w3-light-blue" href="javascript:void(0);" onclick="smallScreenNav()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
 	</li>
 	<li><a href="#home" class="w3-padding-large w3-white">Home</a></li>
-	<li class="w3-hide-small"><a href="#gallery" class="w3-padding-large w3-hover-white w3-text-black">Gallery</a></li>
+	<li class="w3-hide-small"><a href="#" class="w3-padding-large w3-hover-white w3-text-black">Gallery</a></li>
 	<li class="w3-hide-small"><a href="#contact" class="w3-padding-large w3-hover-white w3-text-black">Contact Us</a></li>
-	<li class="w3-hide-small"><a href="#hours" class="w3-padding-large w3-hover-white w3-text-black">Opening Hours</a></li>
+	<li class="w3-hide-small"><a href="#" class="w3-padding-large w3-hover-white w3-text-black">Opening Hours</a></li>
 </div>
 </ul>
 
 <!-- Navbar on small screens -->
 <div id="idNav" class="w3-hide w3-hide-large w3-hide-medium w3-top" style="margin-top:51px">
 	<ul class="w3-navbar w3-left-align w3-large w3-light-grey">
-		<li><a class="w3-padding-large w3-text-black" href="#gallery">Gallery</a></li>
+		<li><a class="w3-padding-large w3-text-black" href="#">Gallery</a></li>
 		<li><a class="w3-padding-large w3-text-black" href="#contact">Contact Us</a></li>
-		<li><a class="w3-padding-large w3-text-black" href="#hours">Opening Hours</a></li>
+		<li><a class="w3-padding-large w3-text-black" href="#">Opening Hours</a></li>
 	</ul>
 </div>
 
