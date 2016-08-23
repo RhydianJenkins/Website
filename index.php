cfe05107646228e35dfb92e534a01990699e0a19
rhydianjenkins <rhydz@msn.com>
2016-08-23 20:54:44 +0100

    added map

--- index.php
+++ index.php
@@ -28,6 +28,7 @@
 	<li class="w3-hide-small"><a href="#gallery" class="w3-padding-large w3-hover-white w3-text-black">Gallery</a></li>
 	<li class="w3-hide-small"><a href="#contact" class="w3-padding-large w3-hover-white w3-text-black">Contact Us</a></li>
 	<li class="w3-hide-small"><a href="#hours" class="w3-padding-large w3-hover-white w3-text-black">Opening Hours</a></li>
+</div>
 </ul>
 
 <!-- Navbar on small screens -->
@@ -50,25 +51,49 @@
 <!-- Content start -->
 <!------------------->
 
-<!-- Welcome -->
-<div class="w3-container w3-center w3-padding-32">
-	<h1>Welcome</h1>
-	<p>The club is situated three miles south of Port Talbot, South Wales and was formerly known as the Corus Sailing Club, Margam.</p>
-	<p>The reservoir is over one mile long by approximately half a mile wide, where racing takes place every Sunday throughout the whole year (for those brave enough!) and also on Saturdays, Tuesday evenings and Wednesday evenings through the Spring and Summer months.</p>
+<!-- First Grid -->
+<div class="w3-row-padding w3-padding-64 w3-container">
+	<div class="w3-content">
+		<div class="w3-twothird">
+			<h1>Welcome</h1>
+			<p class="w3-padding-16">The club is situated three miles south of Port Talbot, South Wales and was formerly known as the Corus Sailing Club, Margam.</p>
+
+			<p class="w3-padding-16">The reservoir is over one mile long by approximately half a mile wide, where racing takes place every Sunday throughout the whole year (for those brave enough!) and also on Saturdays, Tuesday evenings and Wednesday evenings through the Spring and Summer months.</p>
+		</div>
+
+		<div class="w3-third w3-center">
+			<img /> <!-- TODO: Welcoming image here -->
+		</div>
+	</div>
+</div>
+
+<!-- Second Grid -->
+<div class="w3-row-padding w3-light-grey w3-padding-64 w3-container">
+	<div class="w3-content">
+		<div class="w3-third w3-center">
+			<?php include(SCRIPTS_PATH . "map.html"); ?>
+		</div>
+		<div class="w3-twothird">
+			<h1>Location</h1>
+			<address>
+				<strong>Tata Steel Sailing Club</strong><br />
+				Eglwys Nunydd Reservoir<br />
+				Off Longlands Lane<br /> <!-- TODO: is this the real address? -->
+				Margam<br />
+				Port Talbot<br />
+				SA13 2NS<br />
+			</address>
+			<p class="w3-padding-16">Tata Steel Sailing Club is located just off Junction 38 of the M4. If traveling from the East, i.e. traveling from the Cardiff direction take the first exit off the J38 roundabout. If traveling from the West, i.e. traveling, from the Swansea direction take the 3rd exit off the J38 roundabout.</p>
+		</div>
+	</div>
 </div>
 
 <!----------------->
 <!-- Content end -->
 <!----------------->
 
-<br /><br /><br /><br /><br /><br /><br /><br /><br />
-<br /><br /><br /><br /><br /><br /><br /><br /><br />
-<br /><br /><br /><br /><br /><br /><br /><br /><br />
-
 <!-- Contact form -->
 <?php include(INC_PATH . "contact_form.php"); ?>
-
-<hr />
 
 <!-- Footer -->
 <footer class="w3-container w3-padding-32 w3-center w3-opacity">
@@ -81,8 +106,8 @@
 	</div>
 </footer>
 
+<!-- Small screen nav menu -->
 <script>
-	// Used to toggle the menu on small screens when clicking on the menu button
 	function smallScreenNav() {
 		var x = document.getElementById("idNav");
 		if (x.className.indexOf("w3-show") == -1) {
@@ -92,7 +117,6 @@
 		}
 	}
 </script>
-
 
 <!-- jquery -->
 <script src="<?php echo JS_PATH . 'jquery.min.js'?>"></script>
@@ -106,4 +130,3 @@
 
 </body>
 </html>
-
