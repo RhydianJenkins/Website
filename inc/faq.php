23a4e64e7f0627427c7db65a23f3874aaa370578
Rhydian <Rhydz@msn.com>
2016-10-27 18:57:45 +0100

    home page styling cahnges, faq questions but no answers yet

--- inc/faq.php
+++ inc/faq.php
@@ -13,5 +13,57 @@
 			</div>
 		</div>
 	</div>
+	
+	<div class="panel panel-default pointer-hover">
+		<div class="panel-heading accordion-toggle collapsed" data-toggle="collapse" data-parent="#faq" href="#collapseTwo">
+			<h4 class="panel-title">
+				<a class="accordion-toggle collapsed" data-toggle="collapse">Where do I apply to join Tata Steel Sailing Club?</a>
+			</h4>
+		</div>
+		<div id="collapseTwo" class="panel-collapse collapse">
+			<div class="panel-body">
+				TODO: answer
+			</div>
+		</div>
+	</div>
+	
+	<div class="panel panel-default pointer-hover">
+		<div class="panel-heading accordion-toggle collapsed" data-toggle="collapse" data-parent="#faq" href="#collapseThree">
+			<h4 class="panel-title">
+				<a class="accordion-toggle collapsed" data-toggle="collapse">What time is Tata Steel Sailing Club open?</a>
+			</h4>
+		</div>
+		<div id="collapseThree" class="panel-collapse collapse">
+			<div class="panel-body">
+				TODO: answer
+			</div>
+		</div>
+	</div>
+
+	<div class="panel panel-default pointer-hover">
+		<div class="panel-heading accordion-toggle collapsed" data-toggle="collapse" data-parent="#faq" href="#collapseFour">
+			<h4 class="panel-title">
+				<a class="accordion-toggle collapsed" data-toggle="collapse">How much does it cost to join the club?</a>
+			</h4>
+		</div>
+		<div id="collapseFour" class="panel-collapse collapse">
+			<div class="panel-body">
+				TODO: answer
+			</div>
+		</div>
+	</div>
+	
+	<div class="panel panel-default pointer-hover">
+		<div class="panel-heading accordion-toggle collapsed" data-toggle="collapse" data-parent="#faq" href="#collapseFive">
+			<h4 class="panel-title">
+				<a class="accordion-toggle collapsed" data-toggle="collapse">How much does it cost to join the club?</a>
+			</h4>
+		</div>
+		<div id="collapseFive" class="panel-collapse collapse">
+			<div class="panel-body">
+				TODO: answer
+			</div>
+		</div>
+	</div>
 
 </div>
