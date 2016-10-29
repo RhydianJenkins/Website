d283098532b33fd1eb982c6fbd4a1244bf75a87b
rhydianjenkins <rhydz@msn.com>
2016-10-29 14:47:31 +0100

    WIP: introduce years into the results page

--- inc/results_sidebar.php
+++ inc/results_sidebar.php
@@ -1,19 +1,29 @@
-<!-- TODO: text wrap not perfect on long file names -->
+<?php
+	// TODO: text wrap not perfect on long file names 
+
+	$years = array_diff(scandir(RESULTS_PATH), array('.', '..'));
+	foreach($years as $year) {
+		$allFiles = scandir(RESULTS_PATH.'/'.$year);
+		$filteredFiles = array_diff($allFiles, array('.', '..'));
+		$filteredFiles = array_values($filteredFiles);
+		$results[$year] = $filteredFiles;
+	}
+	
+	$results = array_reverse($results, true);
+	//echo'<pre>';var_dump($results); die();
+?>
 <div id="results-side-nav" class="small visible-lg">
 	<nav>
 		<ul class="nav nav-list">
 		
-		<?php
-		
-			$resultsFiles = array_diff(scandir(RESULTS_PATH), array('.', '..'));
-			// go through each file and 
-			foreach($resultsFiles as $file) {
-				$fileName = ucwords(substr(str_replace('_', ' ', $file), 0, strrpos(str_replace('_', ' ', $file), "."))); // make it look pretty
-				echo "<li><a href=\"?target=".$file."\">".$fileName."</a></li>";
-			}
-
-		?>
-
+			<?php foreach($results as $year) : ?>
+				<li><ul class="nav nav-list">
+				<?php foreach($year as $result) : ?>
+					<?php $resultName = ucwords(substr(str_replace('_', ' ', $result), 0, strrpos(str_replace('_', ' ', $result), "."))); ?>
+					<li><a href="?target=<?= $result ?>"><?= $resultName ?></a></li>
+				<?php endforeach ; ?>
+				</ul></li>
+			<?php endforeach ; ?>
 
 			<li><a href="?target=instructions">Sailing Instructions</a></li>
 			<li id="back-to-top" style="display: none;"><a href="#top">Back to top</a></li>
