e27e36a88fd21cd9e9d4c8d1218abe4ff8f3ea18
andrewvjenkins <andrew.v.jenkins@ntlworld.com>
2016-11-19 20:37:22 +0000

    updated faq

--- inc/faq.php
+++ inc/faq.php
@@ -9,7 +9,7 @@
 		</div>
 		<div id="collapseOne" class="panel-collapse collapse">
 			<div class="panel-body">
-				No, anyone can join the club - not just Tata Steel employees!
+				No, anyone can join the club - not just Tata Steel employees! Associate Members make up the ulk of the club's membership
 			</div>
 		</div>
 	</div>
@@ -22,7 +22,7 @@
 		</div>
 		<div id="collapseTwo" class="panel-collapse collapse">
 			<div class="panel-body">
-				TODO: answer
+				You can download an application form from this website or pick one up from the club. You are welcome to the club at any time we are open. Please look at the calendar to see when we are there
 			</div>
 		</div>
 	</div>
@@ -30,12 +30,12 @@
 	<div class="panel panel-default pointer-hover">
 		<div class="panel-heading accordion-toggle collapsed" data-toggle="collapse" data-parent="#faq" href="#collapseThree">
 			<h4 class="panel-title">
-				<a class="accordion-toggle collapsed" data-toggle="collapse">What time is Tata Steel Sailing Club open?</a>
+				<a class="accordion-toggle collapsed" data-toggle="collapse">When is Tata Steel Sailing Club open?</a>
 			</h4>
 		</div>
 		<div id="collapseThree" class="panel-collapse collapse">
 			<div class="panel-body">
-				TODO: answer
+				Times and dates vary through the seasons. Might be an idea to contact a member first and arrange a visit, although you are very welcome to just turn up at any time we're there
 			</div>
 		</div>
 	</div>
@@ -48,7 +48,7 @@
 		</div>
 		<div id="collapseFour" class="panel-collapse collapse">
 			<div class="panel-body">
-				TODO: answer
+				Membership fees very depending on your circumstances. Download the application form, it details the various offers
 			</div>
 		</div>
 	</div>
@@ -56,12 +56,12 @@
 	<div class="panel panel-default pointer-hover">
 		<div class="panel-heading accordion-toggle collapsed" data-toggle="collapse" data-parent="#faq" href="#collapseFive">
 			<h4 class="panel-title">
-				<a class="accordion-toggle collapsed" data-toggle="collapse">How much does it cost to join the club?</a>
+				<a class="accordion-toggle collapsed" data-toggle="collapse">Are there any age restrictions?</a>
 			</h4>
 		</div>
 		<div id="collapseFive" class="panel-collapse collapse">
 			<div class="panel-body">
-				TODO: answer
+				We've had sailing members from 5 years old to 85. Physical fitness is more important than age. Sailing is one of those hobbies that suites all. You just need to pick the boat that suites you.
 			</div>
 		</div>
 	</div>
