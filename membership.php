b06f8bf46a621e2d6c9adf84c4a2009b3440f42a
rhydianjenkins <rhydz@msn.com>
2016-11-29 20:33:43 +0000

    add membership, about, and useful links pages

---
+++ membership.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php require("config.php"); ?>
+<title><?php echo SITE_NAME; ?></title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- w3 template css -->
+<link rel="stylesheet" type="text/css" href="<?php echo STYLES_PATH . "w3c.css"; ?>" />
+<!-- awesome fonts css -->
+<link rel="stylesheet" type="text/css" href="<?php echo STYLES_PATH . "font-awesome.min.css"; ?>" />
+<!-- bootstrap 3.3.7 css -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<!-- custom css -->
+<link rel="stylesheet" type="text/css" href="<?php echo STYLES_PATH . "custom-main.css"; ?>" />
+<!-- jQuery -->
+<script src="<?php echo JS_PATH . 'jquery.min.js'?>"></script>
+</head>
+<body>
+
+<!-- Navbar -->
+<?php include INC_PATH . 'bootstrap_navbar.php'; ?>
+
+<!-- Page -->
+<?php include INC_PATH . 'pages/membership.page.php'; ?>
+
+<!-- Footer -->
+<?php include INC_PATH . 'footer.php'; ?>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="<?php echo JS_PATH . 'bootstrap.min.js' ?>"></script>
+
+</body>
+</html>
