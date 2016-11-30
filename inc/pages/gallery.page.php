03ed59bedfe3be1952469e69f2f257333fef6298
rhydianjenkins <rhydz@msn.com>
2016-11-30 20:16:07 +0000

    gallery page progress

--- inc/pages/gallery.page.php
+++ inc/pages/gallery.page.php
@@ -6,6 +6,7 @@ Make sure all thumbnail images are the same size (preferebly cast to 400x300 px)
 Make a script that generates the slides. Can't ahve all the albums on the same page - it will be a mess
 Maybe look into disabling bootstraps autoscroll
 Maybe look into getting the image to fit the frame on larger screens perfectly? (margin-0-auto is a tmp fix)
+BUG: when you click on an image, all other images are replaced by that one.
 
 -->
 
@@ -19,62 +20,62 @@ Maybe look into getting the image to fit the frame on larger screens perfectly? 
 
         <div class="col-lg-3 col-md-4 col-xs-6 thumb pointer-hover">
             <a class="thumbnail">
-                <img class="img-responsive" src="http://placehold.it/400x300" data-toggle="modal" data-target="#myModal" alt="">
+                <img class="img-responsive center-block" src="http://placehold.it/400x300" data-toggle="modal" data-target="#myModal" alt="">
             </a>
         </div>
         <div class="col-lg-3 col-md-4 col-xs-6 thumb pointer-hover">
             <a class="thumbnail">
-                <img class="img-responsive" src="http://placehold.it/400x300" data-toggle="modal" data-target="#myModal" alt="">
+                <img class="img-responsive center-block" src="http://placehold.it/400x300" data-toggle="modal" data-target="#myModal" alt="">
             </a>
         </div>
         <div class="col-lg-3 col-md-4 col-xs-6 thumb pointer-hover">
             <a class="thumbnail">
-                <img class="img-responsive" src="http://placehold.it/400x300" data-toggle="modal" data-target="#myModal" alt="">
+                <img class="img-responsive center-block" src="http://placehold.it/400x300" data-toggle="modal" data-target="#myModal" alt="">
             </a>
         </div>
         <div class="col-lg-3 col-md-4 col-xs-6 thumb pointer-hover">
             <a class="thumbnail">
-                <img class="img-responsive" src="http://placehold.it/400x300" data-toggle="modal" data-target="#myModal" alt="">
+                <img class="img-responsive center-block" src="http://placehold.it/400x300" data-toggle="modal" data-target="#myModal" alt="">
             </a>
         </div>
         <div class="col-lg-3 col-md-4 col-xs-6 thumb pointer-hover">
             <a class="thumbnail">
-                <img class="img-responsive" src="http://placehold.it/400x300" data-toggle="modal" data-target="#myModal" alt="">
+                <img class="img-responsive center-block" src="http://placehold.it/400x300" data-toggle="modal" data-target="#myModal" alt="">
             </a>
         </div>
         <div class="col-lg-3 col-md-4 col-xs-6 thumb pointer-hover">
             <a class="thumbnail">
-                <img class="img-responsive" src="http://placehold.it/400x300" data-toggle="modal" data-target="#myModal" alt="">
+                <img class="img-responsive center-block" src="http://placehold.it/400x300" data-toggle="modal" data-target="#myModal" alt="">
             </a>
         </div>
         <div class="col-lg-3 col-md-4 col-xs-6 thumb pointer-hover">
             <a class="thumbnail">
-                <img class="img-responsive" src="http://placehold.it/400x300" data-toggle="modal" data-target="#myModal" alt="">
+                <img class="img-responsive center-block" src="http://placehold.it/400x300" data-toggle="modal" data-target="#myModal" alt="">
             </a>
         </div>
         <div class="col-lg-3 col-md-4 col-xs-6 thumb pointer-hover">
             <a class="thumbnail">
-                <img class="img-responsive" src="http://placehold.it/400x300" data-toggle="modal" data-target="#myModal" alt="">
+                <img class="img-responsive center-block" src="http://placehold.it/400x300" data-toggle="modal" data-target="#myModal" alt="">
             </a>
         </div>
         <div class="col-lg-3 col-md-4 col-xs-6 thumb pointer-hover">
             <a class="thumbnail">
-                <img class="img-responsive" src="http://placehold.it/400x300" data-toggle="modal" data-target="#myModal" alt="">
+                <img class="img-responsive center-block" src="http://placehold.it/400x300" data-toggle="modal" data-target="#myModal" alt="">
             </a>
         </div>
         <div class="col-lg-3 col-md-4 col-xs-6 thumb pointer-hover">
             <a class="thumbnail">
-                <img class="img-responsive" src="http://placehold.it/400x300" data-toggle="modal" data-target="#myModal" alt="">
+                <img class="img-responsive center-block" src="http://placehold.it/400x300" data-toggle="modal" data-target="#myModal" alt="">
             </a>
         </div>
         <div class="col-lg-3 col-md-4 col-xs-6 thumb pointer-hover">
             <a class="thumbnail">
-                <img class="img-responsive" src="http://placehold.it/400x300" data-toggle="modal" data-target="#myModal" alt="">
+                <img class="img-responsive center-block" src="http://placehold.it/400x300" data-toggle="modal" data-target="#myModal" alt="">
             </a>
         </div>
         <div class="col-lg-3 col-md-4 col-xs-6 thumb pointer-hover">
             <a class="thumbnail">
-                <img class="img-responsive" src="http://placehold.it/400x300" data-toggle="modal" data-target="#myModal" alt="">
+                <img class="img-responsive center-block" src="http://placehold.it/400x300" data-toggle="modal" data-target="#myModal" alt="">
             </a>
         </div>
     </div>
@@ -82,10 +83,10 @@ Maybe look into getting the image to fit the frame on larger screens perfectly? 
 </div><!-- /.container -->
 
 <div id="myModal" class="modal fade" role="dialog">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-body">
-                <img class="showimage img-responsive" src="" />
+                <img class="showimage img-responsive center-block" style="width: 100%; height: 100%;" src="" />
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
