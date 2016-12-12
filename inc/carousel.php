a91ad6ec7d40f52e6407111fb61fba1e7a851cc8
Rhydian Jenkins <rhydian.jenkins@lexisnexis.com>
2016-12-12 18:00:54 +0000

    home page progress: carousel now reads file structure and is no longer hard coded

--- inc/carousel.php
+++ inc/carousel.php
@@ -1,19 +1,23 @@
+<?php
+    $carouselImages = scandir(IMGS_PATH . 'carousel');
+    $carouselImages = array_values(preg_grep('/\.(jpg|jpeg|png|gif)(?:[\?\#].*)?$/i', $carouselImages));
+?>
 <!-- Full Page Image Background Carousel Header -->
 <div id="myCarousel" class="carousel slide">
     <!-- Indicators -->
     <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <?php foreach($carouselImages as $key => $carouselImage) : ?>
+            <li data-target="#myCarousel" data-slide-to="<?= $key ?>" <?= ($key === 0) ? 'class="active"' : '' ; ?>></li>
+        <?php endforeach ; ?>
     </ol>
 
     <!-- Wrapper for Slides -->
     <div class="carousel-inner">
-        <div class="item active">
-            <div class="fill" style="background-image:url('<?= IMGS_PATH . 'carousel/boats2.jpg' ?>');"></div>
-        </div>
-        <div class="item">
-            <div class="fill" style="background-image:url('<?= IMGS_PATH . 'carousel/boats3.jpg' ?>');"></div>
-        </div>
+        <?php foreach($carouselImages as $key => $carouselImage) : ?>
+            <div class="item<?= ($key === 0) ? ' active' : '' ; ?>">
+                <div class="fill" style="background-image:url('<?= IMGS_PATH . 'carousel/' . $carouselImage; ?>');"></div>
+            </div>
+        <?php endforeach ; ?>
     </div>
 
     <!-- Controls -->
