137d235e05f8b3a1992bf3aa238857d32c60f131
Rhydian Jenkins <rhydian.jenkins@lexisnexis.com>
2016-12-15 18:13:44 +0000

    home page tidy up

--- inc/pages/home.page.php
+++ inc/pages/home.page.php
@@ -14,20 +14,9 @@
 <!-- Page start anchor -->
 <div id="anchor"></div>
 
-<!-- Empty -->
+<!-- Info -->
 <div id="first-content" class="container-fluid border-top-3" style="background-color: rgba(255, 255, 255, 1); min-height: 400px">
-	<div class="container padding-20">
-		<div class="row">
-			<div class="col-lg-6">
-				<h1 class="page-header">Sailing</h1>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-			</div>
-			<div class="col-lg-6">
-				<h1 class="page-header">Windsurfing</h1>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-			</div>
-		</div>
-	</div>
+	<?php include INC_PATH . 'front-page-info.php'; ?>
 </div>
 
 <!-- Carousel -->
@@ -37,17 +26,7 @@
 
 <!-- News -->
 <div id="first-content" class="container-fluid border-top-3" style="background-color: rgba(255, 255, 255, 1); min-height: 400px">
-	<div class="container padding-20">
-        <div class="jumbotron">
-            <h1>News!</h1>
-        </div>
-		<h1 class="page-header">Some news</h1>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <h1 class="page-header">Some more news</h1>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <h1 class="page-header">And even more news</h1>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-	</div>
+	<?php include INC_PATH . 'news.php'; ?>
 </div>
 
 <!-- Location -->
@@ -57,16 +36,12 @@
 
 <!-- FAQ -->
 <div class="container-fluid border-top-3" style="background-color: rgba(0, 0, 0, 0.6); min-height: 400px;">
-	<div class="container padding-20">
-		<?php include INC_PATH . 'faq.php'; ?>
-	</div>
+	<?php include INC_PATH . 'faq.php'; ?>
 </div>
 
 <!-- Facebook feed -->
 <div class="container-fluid border-top-3" style="background-color: rgb(255, 255, 255); min-height: 400px;">
-	<div class="container">
-		<?php include INC_PATH . 'facebook.php'; ?>
-	</div>
+	<?php include INC_PATH . 'facebook.php'; ?>
 </div>
 
 <!-- Blur bg image and scroll arrow opacity on scroll -->
