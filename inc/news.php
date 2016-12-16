d35690088d74d79fd5e179603b115a82e2d733ae
rhydianjenkins <rhydz@msn.com>
2016-12-16 20:57:32 +0000

    front page progress

--- inc/news.php
+++ inc/news.php
@@ -1,11 +1,10 @@
 <div class="container padding-20">
-    <div class="jumbotron">
-        <h1>News!</h1>
-    </div>
-    <h1 class="page-header">Some news</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    <h1 class="page-header">Some more news</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    <h1 class="page-header">And even more news</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <div class="jumbotron"><h1>News</h1></div>
+
+    <h1 class="page-header">OOD Rota For 2017</h1>
+    <p>A blank paper schedule can be found in the club for members to enter dates that suit them, please select 3 dates each that suit you as this will be taken down in December after which a link to an online electronic OOD system will be issued and open for a further two weeks. OOD rota will close end of December and members names missing will be entered by the scheduler.</p>
+    <p>Without knowing your choice, you duties will be allocated and you will ned to arrange your own substitues once the rota is finalised. Please make every effort to select dates that suit you. Unfortunately this year OOD attendance has been disappointing and has meant other members giving up their own time on the water to cover those members who fail to attend. "Undertaking OOD is requirement of membership and could effect your membership going forward".</p>
+
+    <h1 class="page-header">2017 Membership</h1>
+    <p>2017 Application Forms are available on the Membership Page. Please complete the form found there, or email a completed copy to the Membership Secretary at membership@tatasteelsailing.org.uk who will advise you on fee payment options.</p>
 </div>
