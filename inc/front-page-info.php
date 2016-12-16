aa6816175de2275b5a3978e0705e7c96c598161a
rhydianjenkins <rhydz@msn.com>
2016-12-16 22:26:51 +0000

    front page polishing

--- inc/front-page-info.php
+++ inc/front-page-info.php
@@ -5,7 +5,8 @@
             <div class="text-center">
                 <img class="img-circle info-img" src="<?= IMGS_PATH . 'front-page-sailing.jpg'; ?>" />
             </div>
-            <p>The reservoir is over one mile long by approximately half a mile wide, where racing takes place every Sunday throughout the whole year (for those brave enough!) and also on Saturdays, Tuesday evenings and Wednesday evenings through the Spring and Summer months.</p>
+            <p>The reservoir is over one mile long by approximately half a mile wide, where racing takes place every Sunday throughout the whole year (for those brave enough!) 
+                and also on Saturdays, Tuesday evenings and Wednesday evenings through the Spring and Summer months.</p>
         </div>
         <div class="col-md-4 padding-20">
             <h1 class="page-header text-center">Windsurfing</h1>
@@ -14,14 +15,13 @@
             </div>
             <p>Whatever your ability, the Lake is ideal for windsurfing in most wind directions, with a spacious grassy area for rigging, and an easy launching area.
             You don’t have to worry about tides and you can enjoy the luxury of hot showers, hot drinks and the chance to have a chat with other members.</p>
-            <p>The lake is open for windsurfing on Sundays only from October to mid March;  in the Spring and Summer we’re also open on Saturdays and Tuesday and Wednesday evenings (see club booklet).</p>
         </div>
         <div class="col-md-4 padding-20">
             <h1 class="page-header text-center">Social</h1>
             <div class="text-center">
                 <img class="img-circle info-img" src="<?= IMGS_PATH . 'front-page-social.jpg'; ?>" />
             </div>
-            <p>Stuff and things.</p>
+            <p>TODO</p>
         </div>
     </div>
 </div>
