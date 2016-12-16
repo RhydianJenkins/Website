7ad6bba5ea7c691cd4b74669c05a2eaa0c0595f6
rhydianjenkins <rhydz@msn.com>
2016-12-16 21:12:40 +0000

    front page progress

--- inc/front-page-info.php
+++ inc/front-page-info.php
@@ -1,28 +1,27 @@
 <div class="container padding-20">
     <div class="row">
-        <div class="col-md-4" style="padding-bottom: 20px;">
-            <h1 class="page-header">Sailing</h1>
-            <div class="text-center" style="width: 100%;">
-                <img class="img-circle fill info-img" src="http://placehold.it/250x250" />
+        <div class="col-md-4 padding-20">
+            <h1 class="page-header text-center">Sailing</h1>
+            <div class="text-center">
+                <img class="img-circle info-img" src="<?= IMGS_PATH . 'front-page-sailing.jpg'; ?>" />
             </div>
             <p>The reservoir is over one mile long by approximately half a mile wide, where racing takes place every Sunday throughout the whole year (for those brave enough!) and also on Saturdays, Tuesday evenings and Wednesday evenings through the Spring and Summer months.</p>
         </div>
-        <div class="col-md-4" style="padding-bottom: 20px;">
-            <h1 class="page-header">Windsurfing</h1>
-            <div class="text-center" style="width: 100%;">
-                <img class="img-circle fill info-img" src="http://placehold.it/250x250" />
+        <div class="col-md-4 padding-20">
+            <h1 class="page-header text-center">Windsurfing</h1>
+            <div class="text-center">
+                <img class="img-circle info-img" src="<?= IMGS_PATH . 'front-page-windsurfing.jpg'; ?>" />
             </div>
             <p>Whatever your ability, the Lake is ideal for windsurfing in most wind directions, with a spacious grassy area for rigging, and an easy launching area.
             You don’t have to worry about tides and you can enjoy the luxury of hot showers, hot drinks and the chance to have a chat with other members.</p>
             <p>The lake is open for windsurfing on Sundays only from October to mid March;  in the Spring and Summer we’re also open on Saturdays and Tuesday and Wednesday evenings (see club booklet).</p>
-            <p>It's important that you hold the combination lock closed and spin the numerals when entering or exiting the reservoir outer gate.</p>
         </div>
-        <div class="col-md-4" style="padding-bottom: 20px;">
-            <h1 class="page-header">Social</h1>
-            <div class="text-center" style="width: 100%;">
-                <img class="img-circle fill info-img" src="http://placehold.it/250x250" />
+        <div class="col-md-4 padding-20">
+            <h1 class="page-header text-center">Social</h1>
+            <div class="text-center">
+                <img class="img-circle info-img" src="<?= IMGS_PATH . 'front-page-social.jpg'; ?>" />
             </div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>Stuff and things.</p>
         </div>
     </div>
 </div>
