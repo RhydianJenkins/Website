01eb1392c89b98ed4e95f662303bde1d705a4832
andrewvjenkins <andrew.v.jenkins@tlworld.co.uk>
2016-12-20 20:17:11 +0000

    news updated

--- inc/news.php
+++ inc/news.php
@@ -2,8 +2,8 @@
     <div class="jumbotron"><h1>News</h1></div>
 
     <h1 class="page-header">OOD Rota For 2017 <small>11/11/2011</small></h1>
-    <p>A blank paper schedule can be found in the club for members to enter dates that suit them, please select 3 dates each that suit you as this will be taken down in December after which a link to an online electronic OOD system will be issued and open for a further two weeks. OOD rota will close end of December and members names missing will be entered by the scheduler.</p>
-    <p>Without knowing your choice, you duties will be allocated and you will ned to arrange your own substitues once the rota is finalised. Please make every effort to select dates that suit you. Unfortunately this year OOD attendance has been disappointing and has meant other members giving up their own time on the water to cover those members who fail to attend. "Undertaking OOD is requirement of membership and could effect your membership going forward".</p>
+    <p>An on-line portal has been set up to llow members to select and manage their own Officer of the Day duties. The portal can be found at <a href= "https://apex.oracle.com/pls/apex/f?p=76809:1" target="_blank">Member's OOD Portal</a>. Please select 3 dates each that suit you. OOD rota will close end of December and members names missing will be entered by the scheduler.</p>
+    <p>Without knowing your choice, you duties will be allocated and you will ned to arrange your own substitues once the rota is finalised. Please make every effort to select dates that suit you.</p>
 
     <h1 class="page-header">2017 Membership <small>11/11/2011</small></h1>
     <p>2017 Application Forms are available on the Membership Page. Please complete the form found there, or email a completed copy to the Membership Secretary at membership@tatasteelsailing.org.uk who will advise you on fee payment options.</p>
