127d888390905beebb86ba50f97b4bd6d8e3f344
rhydianjenkins <rhydz@msn.com>
2017-01-02 17:13:19 +0000

    container and container-fluid are not nestable so have been removed from the front page

--- inc/pages/home.page.php
+++ inc/pages/home.page.php
@@ -14,32 +14,32 @@
 <div id="anchor"></div>
 
 <!-- Info -->
-<div id="first-content" class="container-fluid border-top-3" style="background-color: rgba(255, 255, 255, 1); min-height: 400px">
+<div id="first-content" class="border-top-3" style="background-color: rgba(255, 255, 255, 1); min-height: 400px">
 	<?php include INC_PATH . 'front-page-info.php'; ?>
 </div>
 
 <!-- Carousel -->
-<div class="container-fluid border-top-3" style="height: 800px; padding-left: 0; padding-right: 0;">
+<div class="border-top-3" style="height: 800px; padding-left: 0; padding-right: 0;">
 	<?php include INC_PATH . 'carousel.php'; ?>
 </div>
 
 <!-- News -->
-<div id="first-content" class="container-fluid border-top-3" style="background-color: rgba(255, 255, 255, 1); min-height: 400px">
+<div id="first-content" class="border-top-3" style="background-color: rgba(255, 255, 255, 1); min-height: 400px">
 	<?php include INC_PATH . 'news.php'; ?>
 </div>
 
 <!-- Location -->
-<div class="container-fluid border-top-3" style="height: 800px; padding-left: 0; padding-right: 0;">
+<div class="border-top-3" style="height: 800px; padding-left: 0; padding-right: 0;">
 	<?php include(INC_PATH . "map.html"); ?>
 </div>
 
 <!-- FAQ -->
-<div class="container-fluid border-top-3" style="background-color: rgba(0, 0, 0, 0.6); min-height: 400px;">
+<div class="border-top-3" style="background-color: rgba(0, 0, 0, 0.6); min-height: 400px;">
 	<?php include INC_PATH . 'faq.php'; ?>
 </div>
 
 <!-- Facebook feed -->
-<div class="container-fluid border-top-3" style="background-color: rgb(255, 255, 255); min-height: 400px;">
+<div class="border-top-3" style="background-color: rgb(255, 255, 255); min-height: 400px;">
 	<?php include INC_PATH . 'facebook.php'; ?>
 </div>
 
