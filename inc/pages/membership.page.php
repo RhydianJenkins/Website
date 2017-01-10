59d1aeb9ceb8dc835d3908a50b351bdd872a8dfb
Rhydian <Rhydz@msn.com>
2017-01-10 17:16:35 +0000

    membership page shuffle

--- inc/pages/membership.page.php
+++ inc/pages/membership.page.php
@@ -5,13 +5,6 @@
 </div>
 
 <div class="container">
-    
-    <p>The Club welcomes experienced sailors and those wanting to give sailing a try, young and old. Membership is open to Tata Steel employees and members of the general public. The rates for Tata Steel employee and non Tata Steel employee membership (associate membership) is slightly different. Please select the appropriate button to display the membership forms and fees for each type of membership. Note that membership fees include a compound space for your dinghy or windsurfer storage.</p>
-    
-    <p>The documents are available in pdf or MS Word format for you to view and print off. It would be helpful to the membership secretary if the forms can be printed double sided. The forms are applicable to membership starting from 1st January.&nbsp;You can also simply download and emial the completed form to the club membership secretary, who can advise you of the best method for you to sette the fee</p>        
-
-    <p align="left">If you have an queries then please contact the membership secretary:</p>
-    <strong>email: <a href="mailto:membership@tatasteelsailing.org.uk">membership@tatasteelsailing.org.uk</a></strong></p>      
 
     <h1 class="page-header">Application Forms</h1>
     
@@ -30,5 +23,16 @@
     <p>Completed membership forms can be sent to the membership secretary (<a href="mailto:membership@tatasteelsailing.org.uk">membership@tatasteelsailing.org.uk</a>) or you are welcome to come along and meet us at the club.</p>
     
     <h3><strong>Please note: by becoming a member at Tata Steel Sailing you understand and accept the <a href="?page=healthandsafety" target="_blank">Terms and Conditions</a></strong></h3>
+
+</div>
+
+<div class="container">
+    
+    <p>The Club welcomes experienced sailors and those wanting to give sailing a try, young and old. Membership is open to Tata Steel employees and members of the general public. The rates for Tata Steel employee and non Tata Steel employee membership (associate membership) is slightly different. Please select the appropriate button to display the membership forms and fees for each type of membership. Note that membership fees include a compound space for your dinghy or windsurfer storage.</p>
+    
+    <p>The documents are available in pdf or MS Word format for you to view and print off. It would be helpful to the membership secretary if the forms can be printed double sided. The forms are applicable to membership starting from 1st January.&nbsp;You can also simply download and emial the completed form to the club membership secretary, who can advise you of the best method for you to sette the fee</p>        
+
+    <p align="left">If you have an queries then please contact the membership secretary:</p>
+    <strong>email: <a href="mailto:membership@tatasteelsailing.org.uk">membership@tatasteelsailing.org.uk</a></strong></p>      
 
 </div>
