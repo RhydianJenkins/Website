d836ef850a67d9422f48d33526854bfbb8c92889
Rhydian <Rhydz@msn.com>
2017-01-11 19:47:14 +0000

    Footer added

--- inc/footer.php
+++ inc/footer.php
@@ -1,2 +1,22 @@
-<footer>
+<hr class="margin-0" />
+
+<footer style="background-color: rgba(0, 0, 0, 0.6)">
+    <div class="row row-centered padding-20 text-white">
+        <div class="col-lg-3 col-md-3 col-sm-3 vcenter">
+            <img class="img-circle footer-img" src="<?= IMGS_PATH . 'logo.jpg'; ?>" alt="Club logo">
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-3 text-center vcenter">
+            <h3>Tata Steel Sailing Club</h3>
+            <address>
+                Eglwys Nunydd Reservoir<br />
+                Off Longlands Lane<br />
+                Margam<br />
+                Port Talbot<br />
+                SA13 2NS
+            </address>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-3 vcenter">
+            <img class="img-circle footer-img" src="<?= IMGS_PATH . 'RYA_TC_Logo.gif'; ?>" alt="RYA Approved">
+        </div>
+    </div>
 </footer>
