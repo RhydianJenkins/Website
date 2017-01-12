15566360ce8f123f5793bef37fdb11ec6b43997b
Rhydian <Rhydz@msn.com>
2017-01-12 13:34:35 +0000

    Carousel added to training page

--- inc/pages/training.page.php
+++ inc/pages/training.page.php
@@ -123,10 +123,39 @@
     </div>
 </div>
 
-<!-- Pictures -->
-<div class="container">
-    <h1 class="page-header">Pictures</h1>
-    
+<!-- Carousel -->
+<div class="container-fluid border-top-3" style="height: 800px; padding-left: 0; padding-right: 0; margin-top: 20px;">
+    <?php
+        $carouselImages = scandir(IMGS_PATH . 'gallery/Training');
+        $carouselImages = array_values(preg_grep('/\.(jpg|jpeg|png|gif)(?:[\?\#].*)?$/i', $carouselImages));
+    ?>
+    <!-- Full Page Image Background Carousel Header -->
+    <div id="myCarousel" class="carousel slide">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <?php foreach($carouselImages as $key => $carouselImage) : ?>
+                <li data-target="#myCarousel" data-slide-to="<?= $key ?>" <?= ($key === 0) ? 'class="active"' : '' ; ?>></li>
+            <?php endforeach ; ?>
+        </ol>
+
+        <!-- Wrapper for Slides -->
+        <div class="carousel-inner">
+            <?php foreach($carouselImages as $key => $carouselImage) : ?>
+                <div class="item<?= ($key === 0) ? ' active' : '' ; ?>">
+                    <div class="fill-bottom" style="background-image:url('<?= IMGS_PATH . 'gallery/Training/' . $carouselImage; ?>');"></div>
+                </div>
+            <?php endforeach ; ?>
+        </div>
+
+        <!-- Controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="icon-prev"></span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="icon-next"></span>
+        </a>
+
+    </div>
 </div>
 
 <!-- Hide collapse tabs when clicking another so only one shows at a time -->
