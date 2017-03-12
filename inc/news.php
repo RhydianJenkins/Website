a320fca153b68de89d69d6b20472582cbfcbe8df
rhydianjenkins <rhydz@msn.com>
2017-03-12 17:31:12 +0000

    news style update

--- inc/news.php
+++ inc/news.php
@@ -1,30 +1,40 @@
-<div class="container padding-20">
-    <div class="jumbotron sr1"><h1>News</h1></div>
+<div class="light-grey">
 
-    <div class="sr1">
-        <h1 class="page-header">TATA STEEL SAILING CLUB SOLO OPEN MEETING <small>SAT 15TH APRIL</small></h1>
-        <h2>Schedule</h2>
-		<h4>4 RACES (3 to count)
-        </h4>
-        <ul>BRIEFING 10.30AM</ul>
-        <ul>RACE 1 gun at 11am (approx. 1hour)</ul>
-        <ul>lunch</ul>
-		<ul>Race 2.3.4. (approx. 45 minutes) back to back.</ul>
-		<p>Entry fee £10</p>
-		<p>Nor and SI can be found at <a style="text-decoration: none;" href="<?= DOCS_PATH . '2017SoloNOR.pdf'; ?>" target="_blank"><strong>2017 Solo NOR</strong></a> and <a style="text-decoration: none;" href="<?= DOCS_PATH . '2017SoloSI.pdf'; ?>" target="_blank"><strong>2017 Solo Sailing Instruction</strong></a>.
+    <div class="container padding-20">
+        <div class="jumbotron sr1"><h1>News</h1></div>
 
-	</div><div class="sr1">
-        <h1 class="page-header">OOD Rota For 2017</h1>
-        <p>An on-line portal has been set up to allow members to select and manage their own Officer of the Day duties.
-        </p>
-        <p>The portal can be found at<br> <a href= "https://apex.oracle.com/pls/apex/f?p=76809:1" target="_blank">Member's OOD Portal</a>.
-        </p>
-        <p>Please select 3 dates each that suit you. OOD rota has closed and members names missing will be entered by the scheduler.</p>
-        <p>Without knowing your choice, your duties will be allocated and you will need to arrange your own substitues once the rota is finalised. Please make every effort to select dates that suit you.</p>
+        <div class="sr1 news-container">
+            <h1 class="news-title">TATA STEEL SAILING CLUB SOLO OPEN MEETING <small>SAT 15TH APRIL</small></h1>
+            
+            <h3>4 RACES (3 to count). Entry fee: £10.</h3>
+            <h3>Schedule:</h3>
+            <ul>
+                <li>Briefing 10.30am.</li>
+                <li>Race 1 gun at 11am (approx. 1 hour).</li>
+                <li>Lunch.</li>
+                <li>Race 2, 3, 4 (approx. 45 minutes) back to back.</li>
+            </ul>
+            <p>
+                Nor and SI can be found at 
+                <a style="text-decoration: none;" href="<?= DOCS_PATH . '2017SoloNOR.pdf'; ?>" target="_blank"><strong>2017 Solo NOR</strong></a> and 
+                <a style="text-decoration: none;" href="<?= DOCS_PATH . '2017SoloSI.pdf'; ?>" target="_blank"><strong>2017 Solo Sailing Instructions</strong></a>.
+            </p>
+        </div>
+        
+        <div class="sr1 news-container">
+            <h1 class="news-title">OOD Rota For 2017</h1>
+            
+            <p>An on-line portal has been set up to allow members to select and manage their own Officer of the Day duties.</p>
+            <p>The portal can be found at<br> <a href= "https://apex.oracle.com/pls/apex/f?p=76809:1" target="_blank">Member's OOD Portal</a>.</p>
+            <p>Please select 3 dates each that suit you. OOD rota has closed and members names missing will be entered by the scheduler.</p>
+            <p>Without knowing your choice, your duties will be allocated and you will need to arrange your own substitues once the rota is finalised. Please make every effort to select dates that suit you.</p>
+        </div>
+
+        <div class="sr1 news-container">
+            <h1 class="news-title">2017 Membership</h1>
+            
+            <p>2017 Application Forms are available on the Membership Page. Please complete the form found there, or email a completed copy to the Membership Secretary at membership@tatasteelsailing.org.uk who will advise you on fee payment options.</p>
+        </div>
     </div>
 
-    <div class="sr1">
-        <h1 class="page-header">2017 Membership</h1>
-        <p>2017 Application Forms are available on the Membership Page. Please complete the form found there, or email a completed copy to the Membership Secretary at membership@tatasteelsailing.org.uk who will advise you on fee payment options.</p>
-    </div>
 </div>
