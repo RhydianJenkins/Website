2391dcb1013dcef8dfd9d9f069ec1c73c82f7917
rhydianjenkins <rhydz@msn.com>
2017-03-17 12:31:47 +0000

    Fixed fathers messup. IF YOU DONT PULL THIS DAD I WILL EAT YOUR SOUL

--- inc/news.php
+++ inc/news.php
@@ -1,53 +1,63 @@
 <div class="light-grey">
-
     <div class="container padding-20">
+        
         <div class="jumbotron sr1"><h1>News</h1></div>
 
         <div class="sr1 news-container">
             <h1 class="news-title">TATA STEEL SAILING CLUB SOLO OPEN MEETING <small>SAT 15TH APRIL</small></h1>
-            <p><img src="res/imgs/gallery/2007 Solo Open/Solo.jpg" width="30%" alt="Solo" style="float: right; margin-right: 10px;" hspace="30" vspace="10">
-		</p>
-            <h3>4 RACES (3 to count). Entry fee: £10.</h3>
-            <h3>Schedule:</h3>
-            <ul>
-                <li>Briefing 10.30am.</li>
-                <li>Race 1 gun at 11am (approx. 1 hour).</li>
-                <li>Lunch.</li>
-                <li>Race 2, 3, 4 (approx. 45 minutes) back to back.</li>
-            </ul>
-            <p>
-                Nor and SI can be found at 
-                <a style="text-decoration: none;" href="<?= DOCS_PATH . '2017SoloNOR.pdf'; ?>" target="_blank"><strong>2017 Solo NOR</strong></a> and 
-                <a style="text-decoration: none;" href="<?= DOCS_PATH . '2017SoloSI.pdf'; ?>" target="_blank"><strong>2017 Solo Sailing Instructions</strong></a>.
-            </p>
+            <div class="row">
+                <div class="col-md-6">
+                    <img class="img img-responsive" src="<?= GALLERY_PATH . '2007 Solo Open/Solo.jpg'; ?>" alt="Osprey2015"></img>
+                </div>
+                <div class="col-md-6">
+                    <h3>4 RACES (3 to count). Entry fee: £10.</h3>
+                    <h3>Schedule:</h3>
+                    <ul>
+                        <li>Briefing 10.30am.</li>
+                        <li>Race 1 gun at 11am (approx. 1 hour).</li>
+                        <li>Lunch.</li>
+                        <li>Race 2, 3, 4 (approx. 45 minutes) back to back.</li>
+                    </ul>
+                    <p>
+                        Nor and SI can be found at 
+                        <a style="text-decoration: none;" href="<?= DOCS_PATH . '2017SoloNOR.pdf'; ?>" target="_blank"><strong>2017 Solo NOR</strong></a> and 
+                        <a style="text-decoration: none;" href="<?= DOCS_PATH . '2017SoloSI.pdf'; ?>" target="_blank"><strong>2017 Solo Sailing Instructions</strong></a>.
+                    </p>
+                </div>
+            </div>
         </div>
-        <div class="sr1">
-		<h1 class="page-header">TATA STEEL SAILING CLUB Opsrey OPEN MEETING <small>SAT 20<sup>th</sup> and SUN 21<sup>st</sup> MAY</small></h1>
-        <p><img src="res/imgs/gallery/2015 October Regatta/tata2015_009_22651532445_o.jpg" width="30%" alt="Osprey2015" style="float: left; margin-left: 10px;" hspace="30" vspace="10">
-		</p>
-		<h2>Schedule</h2>
-		<h4>Full schedule, NoR, Sailing Instructions to follow
-        </h4>
-        <ul></ul>
-        <ul>. </ul>
-		<ul>.. </ul>
-		<ul>... </ul>
-		<p>More details to follow </p>
-	</div>
-	<br>
-	<div class="sr1">
-        <h1 class="page-header">OOD Rota For 2017</h1>
-        <p>An on-line portal has been set up to allow members to select and manage their own Officer of the Day duties.
-        </p>
-        <p>The portal can be found at<br> <a href= "https://apex.oracle.com/pls/apex/f?p=76809:1" target="_blank">Member's OOD Portal</a>.
-        </p>
-        <p>Please select 3 dates each that suit you. OOD rota has closed and members names missing will be entered by the scheduler.</p>
-        <p>Without knowing your choice, your duties will be allocated and you will need to arrange your own substitues once the rota is finalised. Please make every effort to select dates that suit you.</p>
-    <div class="sr1 news-container">
+
+        <div class="sr1 news-container">
+            <h1 class="news-title">TATA STEEL SAILING CLUB Opsrey OPEN MEETING <small>SAT 20<sup>th</sup> / SUN 21<sup>st</sup> MAY</small></h1>
+            <div class="row">
+                <div class="col-md-6">
+                    <h2>Schedule</h2>
+                    <h4>Full schedule, NoR, Sailing Instructions to follow</h4>
+                    <ul></ul>
+                    <ul>. </ul>
+                    <ul>.. </ul>
+                    <ul>... </ul>
+                    <p>More details to follow</p>
+                </div>
+
+                <div class="col-md-6">
+                    <img class="img img-responsive" src="<?= GALLERY_PATH . '2015 October Regatta/tata2015_009_22651532445_o.jpg'; ?>" alt="Osprey2015"></img>
+                </div>
+            </div>
+        </div>
+
+        <div class="sr1 news-container">
+            <h1 class="news-title">OOD Rota For 2017</h1>
+            <p>An on-line portal has been set up to allow members to select and manage their own Officer of the Day duties.</p>
+            <p>The portal can be found at<br> <a href= "https://apex.oracle.com/pls/apex/f?p=76809:1" target="_blank">Member's OOD Portal</a>.</p>
+            <p>Please select 3 dates each that suit you. OOD rota has closed and members names missing will be entered by the scheduler.</p>
+            <p>Without knowing your choice, your duties will be allocated and you will need to arrange your own substitues once the rota is finalised. Please make every effort to select dates that suit you.</p>
+        </div>
+
+        <div class="sr1 news-container">
             <h1 class="news-title">2017 Membership</h1>
-            
             <p>2017 Application Forms are available on the Membership Page. Please complete the form found there, or email a completed copy to the Membership Secretary at membership@tatasteelsailing.org.uk who will advise you on fee payment options.</p>
         </div>
-    </div>
 
+    </div>
 </div>
