f5a17c888a8ca42a98f55918660c899cafbda8c7
andrewvjenkins <andrew.v.jenkins@tlworld.co.uk>
2017-03-18 13:19:07 +0000

    Updated news - Osprey Open info published, Push The Boat Out added

--- inc/news.php
+++ inc/news.php
@@ -28,16 +28,21 @@
         </div>
 
         <div class="sr1 news-container">
-            <h1 class="news-title">TATA STEEL SAILING CLUB Opsrey OPEN MEETING <small>SAT 20<sup>th</sup> / SUN 21<sup>st</sup> MAY</small></h1>
+            <h1 class="news-title">TATA STEEL SAILING CLUB Osprey OPEN MEETING <small>SAT 20<sup>th</sup> / SUN 21<sup>st</sup> MAY</small></h1>
             <div class="row">
                 <div class="col-md-6">
                     <h2>Schedule</h2>
-                    <h4>Full schedule, NoR, Sailing Instructions to follow</h4>
-                    <ul></ul>
-                    <ul>. </ul>
-                    <ul>.. </ul>
-                    <ul>... </ul>
-                    <p>More details to follow</p>
+                    <ul>
+						<li>Registration opens at 10am</li>
+						<li>Briefing 12:00</li>
+						<li>Race 1 gun not before 13:00</li>
+						<li>Courses will vary depending on wind conditions/direction</li>
+						<li>Nor and SI can be found at 
+                        <a style="text-decoration: none;" href="<?= DOCS_PATH . '2017OspreyNOR.pdf'; ?>" target="_blank"><strong>2017 Osprey NOR</strong></a> and 
+                        <a style="text-decoration: none;" href="<?= DOCS_PATH . '2017OspreySI.pdf'; ?>" target="_blank"><strong>2017 Osprey Sailing Instructions</strong></a></li>
+                    </ul>
+					<p>For further information, please contact David Downs on 07801713481 or <a href="mailto:davdowns@btinternet.com?Subject=2017%20Tata%20Osprey%20Open" target="_top">davdowns@btinternet.com</a>
+					</p>
                 </div>
 
                 <div class="col-md-6">
@@ -46,7 +51,20 @@
             </div>
         </div>
 
-        <div class="sr1 news-container">
+		<div class="sr1 news-container">
+            <h1 class="news-title">Push The Boat Out</h1>
+			<a style="text-decoration: none;" href="<?= DOCS_PATH . 'PTBO2017.pdf'; ?>" target="_blank"><img class="img img-responsive" src="<?= IMGS_PATH . 'PTBOHeader.jpg'; ?>" alt="PTBO2017"></img></a>
+			
+			<p>Come along and have a go at sailing and windsurfing, all ages from 8 years old. See the Club and meet its members to see what we have on offer.
+			</p>
+			<p>Eglwys Nunydd Reservoir, Off Longlands Lane Margam</p>
+			<p>Port Talbot SA13 2NS</p>
+			<p>For more information:</p>
+			<p>email: <a href="mailto:jeremy.martin6@gmail.com?Subject=2017%20Push%20the%20Boat%20Out" target="_top">jeremy.martin6@gmail.com</a>
+			<p>or call: 01656 861092 317</p>
+		</div>
+		
+			<div class="sr1 news-container">
             <h1 class="news-title">OOD Rota For 2017</h1>
             <p>An on-line portal has been set up to allow members to select and manage their own Officer of the Day duties.</p>
             <p>The portal can be found at<br> <a href= "https://apex.oracle.com/pls/apex/f?p=76809:1" target="_blank">Member's OOD Portal</a>.</p>
@@ -56,7 +74,7 @@
 
         <div class="sr1 news-container">
             <h1 class="news-title">2017 Membership</h1>
-            <p>2017 Application Forms are available on the Membership Page. Please complete the form found there, or email a completed copy to the Membership Secretary at membership@tatasteelsailing.org.uk who will advise you on fee payment options.</p>
+            <p>2017 Application Forms are available on the Membership Page. Please complete the form found there, or email a completed copy to the Membership Secretary at <a href="mailto:membership@tatasteelsailing.org.uk?Subject=2017%20Tata%20Steel%20Sailing%20Club%20membership" target="_top">membership@tatasteelsailing.org.uk</a> who will advise you on fee payment options.</p>
         </div>
 
     </div>
