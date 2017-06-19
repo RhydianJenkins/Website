7eb84c4036cbdb5593dc47f5612eb79c7d98735c
andrewvjenkins <andrew.v.jenkins@tlworld.co.uk>
2017-06-19 12:36:17 +0100

    update from Mumbles

--- inc/news.php
+++ inc/news.php
@@ -10,30 +10,33 @@
                     <img class="img img-responsive" src="<?= IMGS_PATH . 'traveller.jpg'; ?>" alt="Traveller2017"></img>
                 </div>
 				<div class="col-md-6">
-                    <h2>Saundersfoot (Coppet Week)</h2>
-                    <p>The first of the travellers series began with Saundersfoot Regatta (Coppet Week) on Sunday 28th May. This regatta is held over 5 days with one race per day. Six Club boats travelled to the event giving a great kick start to the new Club traveller series. The event results can be seen here <a style="text-decoration: none;" href="<?= RESULTS_PATH . '2017/Resources/Coppet 2017 Results.html'; ?>" target="_blank"><strong>Saundersfoot Results</strong></a>
+                    <h2>Mumbles</h2>
+					<p>The Mumbles Regatta was enjoyed by four travelling boats along with Tata member Alex Shaw, who sails at both clubs. The weather and sailing conditions were what dreams were made of with a solid F3 wind and bright sunshine greeting all sailors on the Saturday.
 					</p>
-					<p>The Series results can be followed from the Results Page</p>
-					<p>It was a mixed week weather wise and the courses were usually a big triangle consisting of a beat followed by two reaches, sometimes kiteable sometimes not
+					<p>The first race got off to a start first time, although Andrew and Rebecca missed the start sequence and Dave and Bill missing the whole race due to a rigging issue after launch. The wind shift on the left side of the beat was taken advantage of by all boats with those making use of the slight pin bias bumping them up the fleet resulting in a close call on the windward mark
 					</p>
-					<p>The start lines were interesting, it was a GH fleet with 75 boats entered and it was great to see toppers starting alongside IC Canoes and Lasers luffing Ospreys, the nerves and competitiveness of the fleet was evident by the three general recalls in the first race on the Monday (Sundays racing being cancelled due to lack of wind)
+					<p>The reaches were well set giving great racing conditions. Andrew and Rebecca filled the boat up with water during a tack taken too quickly – head faster than body! – giving Gordon and Ruth a clean get away they held for the three lap race
 					</p>
-					<p>The socials during the week were top class and included a dinner ay Wisemans Bridge for all Tata Steel members on Wed evening followed by a beach barbecue late Thurs afternoon
+					<p>Bob was comfortable with the win on handicap after battling it out with the slow handicap boats
 					</p>
-					<p>The next regatta in the Traveller Series is Mumbles on the 17<sup>th</sup> and 18<sup>th</sup> June and it would be great to see a few more boats travelling to enjoy some sea sailing and to show off what talent we have at the Club
+					<p>The second race saw Dave and Bill join in, but the wind was starting to build as the sea breeze gathered momentum. Andrew and Rebecca had a few interesting moments to contend with (Genoa sheets coming undone 10 seconds before the start, spinnaker pole coming apart on the reach, main sheet tangle on the boom) which put them in catch up mode for the whole race, giving Gordon and Ruth a clear path to take the race from Bob in second. Alex and Richard just held Andrew and Rebecca off the finish line with Dave and Bill cursing a crash on the gybe that put them in the water for a cooling dip
 					</p>
-					<p>Hope to see you there
+					<p>Race three saw a slightly bigger course with the wind picking up a little more and the tide now playing a part as well. Andrew and Rebecca went with the RS700 to follow the tide to the right whist others went for the wind bend to the left. It gave the Osprey a lead at the windward mark they were able to hold for the race. Dave and Bill managed to keep upright all through the race to finish a credible 3rd in the regatta race, second in the traveller race behind Bob with the Osprey 3rd.
 					</p>
-					<p align=right><i>Gordon Evans</i>
+					<p>Sunday saw the race officer scooting around the bay in a rib looking for some wind. After over an hour of this, we had to concede to abandoning racing for the Sunday and take a discard from the Saturday results
+					</p>
+					<p>It left Bob in first, with Gordon and Ruth in second. Results can be seen at <a style="text-decoration: none;" href="<?= RESULTS_PATH . '2017/Resources/Mumbles 2017 Results.html'; ?>" target="_blank"><strong>Mumbles Results</strong></a>
+                    </p>
+					<p align=right><i>Andrew Jenkins</i>
 					</p>
 					<p>The series incorporates five sea sailing venues across South Wales. Please contact any of the regular travellers of help or advice on how to get your boat to the various great venues
 					</p>
 					All club members are welcome to join in, If you need any more information on travelling, please email <a href="mailto:webmaster@tatasteelsailing.org.uk?Subject=2017%20Tata%20Traveller%20Series" target="_top">webmaster@tatasteelsailing.org.uk</a> for advice and I'll try to point you in the right direction
 					</p>
                 </div>
-
-                
+ 
             </div>
+		</div>
 		<div class="sr1 news-container">
             <h1 class="news-title">TATA STEEL SAILING CLUB 2017 Regatta <small>29<sup>th</sup> and 30<sup>th</sup> APRIL</small></h1>
             <div class="row">
